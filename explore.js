f4000f103062a3ffa5d2723d889ca85e3a8a4b4b
Nirmal <nirmalgaur814@gmail.com>
2021-11-02 17:53:38 +0530

    edits

--- explore.js
+++ explore.js
@@ -2,14 +2,14 @@ const Twilio = require("twilio");
 
 const client = new Twilio(
   "AC96762b5bc14b6832afda9df52dbf9ae2",
-  "43d4b562d3df014e38e1e4904379733a"
+  "735bf0884168cb8d2351dde72adcdd88"
 );
 
 //sending yourself an SMS message:
 console.log("Sending the message...");
 client.messages
   .create({
-    body: "Hello from Node 3",
+    body: "Hello from Node",
     to: "+919548200532", // Text this number
     from: "+14178841125", // From a valid Twilio number
   })
@@ -25,10 +25,4 @@ client.messages
   .then((messages) =>
     console.log(`The most recent message is: ${messages[0].body}`)
   )
-  .catch((err) => console.error(err));
-
-//Deleting the last sent message:
-client.messages
-  .list()
-  .then((messages) => messages[0].body.delete())
   .catch((err) => console.error(err));
